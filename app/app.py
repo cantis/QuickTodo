4700c58b272f48de9bc1a220096aece0ed118636
evan.young <evan.young@laiv.ly>
2022-10-03 16:56:06 -0500

    Added .env handling

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import environ
 
 app = Flask(__name__)
+
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
